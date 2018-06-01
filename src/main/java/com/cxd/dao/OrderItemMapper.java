1165622cfe8698f01f4f5b3ea4d212b2e25f46a7
minhao <chenxiaodong19911207@vip.qq.com>
2018-06-01 15:39:24 +0800

    project init commit

---
+++ src/main/java/com/cxd/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.cxd.dao;
+
+import com.cxd.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
